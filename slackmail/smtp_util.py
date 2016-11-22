0aed77ff9eb8ba3a434f53e4703f5f4c96319e16
negokaz <negokaz@gmail.com>
2016-11-22 22:04:42 +0900

    正規表現をパターンごとに分割

--- slackmail/smtp_util.py
+++ slackmail/smtp_util.py
@@ -27,9 +27,11 @@ def error(msg):
 
 def _md_to_slack_format(str):
   # [](url "title") => <url|title>
-  str = re.sub(r'\[\]\((\S*)\s*"(.*)"\)', r'<\1|\2>', str)
-  # [text](url "title") / [text](url) => <url|text>
-  str = re.sub(r'\[(.*)\]\((\S*)\s*.*\)', r'<\2|\1>', str)
+  str = re.sub(r'\[\]\((\S+)\s*"([^"]+)"\)', r'<\1|\2>', str)
+  # [text](url) => <url|text>
+  str = re.sub(r'\[([^\]+]+)\]\((\S+)\)', r'<\2|\1>', str)
+  # [text](url "title") => <url|text>
+  str = re.sub(r'\[([^\]+]+)\]\((\S+)\s*"([^"]+)"\)', r'<\2|\1>', str)
   return str
 
 def _remove_line_break(str):
