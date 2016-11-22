8095483f8b3c2e98c6b2de072088336014c4f797
negokaz <negokaz@gmail.com>
2016-11-22 16:20:38 +0900

    メッセージを読みやすくなるように画像タグを無視したりする

--- slackmail/smtp_util.py
+++ slackmail/smtp_util.py
@@ -25,19 +25,41 @@ def warn(msg):
 def error(msg):
   return echo(msg, fg='red')
 
-def _reduce_message_texts(text, append):
-  maybeText = append.get_payload(decode=True)
+def _html_parser():
+  parser = html2text.HTML2Text()
+  # https://github.com/Alir3z4/html2text/blob/master/docs/usage.md#available-options
+  parser.ignore_images = True
+  parser.ignore_anchors = True
+  parser.ignore_tables = True
+  parser.skip_internal_links = True
+  parser.protext_links = True
+  parser.single_line_break = True
+  parser.re_space = True
+  return parser
+
+# DO NOT PASS MULTIPART MESSAGE !
+def _message_to_text(msg):
   encoding = append.get_content_charset('utf-8')
+  if msg.get_content_maintype() == 'text':
+    if msg.get_content_subtype() == 'plain':
+      return msg.get_payload(decode=True)
+    else:
+      maybe_text = msg.get_payload(decode=True)
+      return  (_html_parser().handle(maybe_text.decode(encoding)) if maybe_text is not None else None)
+  else: # e.g. image/png
+    return None
+
+def _reduce_message_texts(text, append):
+  maybeText = _message_to_text(append)
   return text + (u'\n' + maybeText.decode(encoding) if maybeText is not None else u'')
 
 def _msg_text(msg):
   if msg.is_multipart():
-    text_payloads = filter(lambda msg: msg.get_content_maintype() == 'text', msg.get_payload())
+    text_payloads = filter(lambda m: m.get_content_maintype() == 'text', msg.get_payload())
     return reduce(_reduce_message_texts, text_payloads, u'')
   else:
-    text = msg.get_payload(decode=True)
-    encoding = msg.get_content_charset('utf-8')
-    return (text.decode(encoding) if text is not None else u'')
+    maybe_text = _message_to_text(msg)
+    return (maybe_text.decode(encoding) if maybe_text is not None else u'')
 
 Message.text = _msg_text
 
@@ -62,9 +84,9 @@ def forward_message(mailfrom, rcptto, msg, webhook_url, authorization_token=None
   channel = re.search(r'^([^@]+)@.+$', rcptto).group(1)
   decoded_titles = decode_header(msg['subject'])
   title = reduce(_reduce_encoded_header, decode_header(msg['subject']), u'')
-  formatted_text = html2text.html2text(msg.text())
+  text = msg.text()
   # encode for slack
-  encoded_text = re.sub(r'\n', "\\n", formatted_text)
+  encoded_text = re.sub(r'\n', "\\n", text)
 
   try:
     json_data = json.dumps({
